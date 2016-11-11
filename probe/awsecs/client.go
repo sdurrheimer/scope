5cd2992b4ca28fa5b9acc702abcbf846f5c6c506
Mike Lang <mikelang3000@gmail.com>
2016-11-10 19:11:31 -0800

    wip:

--- probe/awsecs/client.go
+++ probe/awsecs/client.go
@@ -2,13 +2,18 @@ package awsecs
 
 import (
 	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/ec2metadata"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/ecs"
 )
 
-func getClient(region string) {
-
+func getClient() {
 	sess, err := session.NewSession()
+	if err != nil {
+		return err
+	}
+
+	region, err := ec2metadata.New(session).Region()
 	if err != nil {
 		return err
 	}
--- probe/awsecs/reporter.go
+++ probe/awsecs/reporter.go
@@ -1,8 +1,12 @@
 
 package awsecs
 
+import (
+	"net/http"
+)
 
 struct labelInfo {
+	containerID string
 	cluster string
 	taskArn string
 	family string
@@ -12,27 +16,84 @@ func getLabelInfos() []labelInfo {
 	// TODO get labels for all containers and look for com.amazonaws.ecs.{cluster,task-arn,task-definition-family}
 }
 
-func getRegion() string {
-	// TODO GET http://169.254.169.254/<whatever version is latest atm>/dynamic/instance-identity/document,
-	// parse as json, extract .region
-}
-
-// returns a list of services in the cluster
-func getServices(cluster string) []string {
-	// TODO aws list-services with given cluster, extract .serviceArns
-}
-
 // returns a map from deployment ids to service names
-func getDeploymentMap(cluster string) map[string]string {
-	// TODO describe-services with given cluster and getServices()
-	// for each service, for each deployment in service["deployments"],
-	// results[deployment["id"]] = service["serviceName"]
+// cannot fail as it will attempt to deliver partial results, though that may end up being no results
+func getDeploymentMap(client *ecs.ECS, cluster string) map[string]string {
+	results := make(map[string]string)
+	lock := sync.Mutex{} // lock mediates access to results
+
+	group := sync.WaitGroup{}
+
+	err := client.ListServicesPages(
+		&ecs.ListServicesInput{Cluster: &cluster},
+		func(page *ecs.ListServicesOutput, lastPage bool) bool {
+			// describe each page of 10 (the max for one describe command) concurrently
+			group.Add(1)
+			go func() {
+				defer group.Done()
+
+				resp, err := client.DescribeServices(&ecs.DescribeServicesInput{
+					Cluster: &cluster,
+					Services: page.ServiceArns,
+				})
+				if err != nil {
+					// rather than trying to propogate errors up, just log a warning here
+					log.Warnf("Error describing some ECS services, ECS service report may be incomplete: %v", err)
+					return
+				}
+
+				for _, failure := range resp.Failures {
+					// log the failures but still continue with what succeeded
+					log.Warnf("Failed to describe ECS service %s, ECS service report may be incomplete: %s", failure.Arn, failure.Reason)
+				}
+
+				lock.Lock()
+				for _, service := range resp.Services {
+					for _, deployment := range service.Deployments {
+						results[*deployment.Id] = *service.ServiceName
+					}
+				}
+				lock.Unlock()
+			}()
+			return true
+		}
+	)
+	group.Wait()
+
+	if err != nil {
+		// We want to still return partial results if we have any, so just log a warning
+		log.Warnf("Error listing ECS services, ECS service report may be incomplete: %v", err)
+	}
+	return results
 }
 
 // returns a map from task ARNs to deployment ids
-func getTaskDeployments(cluster string, taskArns []string) map[string]string {
-	// TODO describe-tasks with given cluster and task ARNs
-	// for each task, results[task["taskArn"]] = task["startedBy"]
+func getTaskDeployments(client *ecs.ECS, cluster string, taskArns []string) map[string]string, error {
+	taskPtrs := make([]*string, len(taskArns))
+	for _, arn := range taskArns {
+		taskPtrs = append(taskPtrs, &arn)
+	}
+
+	// You'd think there's a limit on how many tasks can be described here,
+	// but the docs don't mention anything.
+	resp, err := client.DescribeTasks(&ecs.DescribeTasksInput{
+		Cluster: &cluster,
+		Tasks: taskPtrs,
+	})
+	if err != nil {
+		return err, nil
+	}
+
+	for _, failure := range resp.Failures {
+		// log the failures but still continue with what succeeded
+		log.Warnf("Failed to describe ECS task %s, ECS service report may be incomplete: %s", failure.Arn, failure.Reason)
+	}
+
+	results := make(map[string]string)
+	for _, task := resp.Tasks {
+		results[*task.TaskArn] = *task.StartedBy
+	}
+	return results
 }
 
 // returns a map from task ARNs to service names
@@ -57,3 +118,4 @@ func getTaskServices(cluster string, taskArns []string) map[string]string {
 
 	return results
 }
+
